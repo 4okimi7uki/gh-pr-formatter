67abd4220631b3404051d6190e18a61db1bc954f
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-11-30 22:36:07 +0900

    feat: add UI frame and color utilities for CLI output

---
+++ internal/ui/color.go
@@ -0,0 +1,12 @@
+package ui
+
+import "github.com/fatih/color"
+
+var (
+	Green       = color.RGB(67, 219, 88)
+	Yellow      = color.RGB(255, 219, 76)
+	Pink        = color.RGB(255, 76, 180)
+	LightPurple = color.RGB(231, 76, 255)
+	Purple      = color.RGB(151, 76, 255)
+	Blue        = color.RGB(103, 73, 255)
+)
