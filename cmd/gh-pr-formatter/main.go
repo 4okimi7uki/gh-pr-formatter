659c7343c820f2a7d00c04ddcc511da0d0b46367
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-12-07 21:36:44 +0900

    implement graphQL logic insted of gh cmd

--- cmd/gh-pr-formatter/main.go
+++ cmd/gh-pr-formatter/main.go
@@ -7,31 +7,25 @@ import (
 	"strings"
 	"time"
 
+	"github.com/4okimi7uki/gh-pr-formatter/internal/client"
 	"github.com/4okimi7uki/gh-pr-formatter/internal/gh"
+	"github.com/4okimi7uki/gh-pr-formatter/internal/pkg/repository"
+	"github.com/4okimi7uki/gh-pr-formatter/internal/pr"
 	"github.com/4okimi7uki/gh-pr-formatter/internal/template"
 	"github.com/4okimi7uki/gh-pr-formatter/internal/ui"
-	"github.com/4okimi7uki/gh-pr-formatter/internal/utils"
 	"github.com/briandowns/spinner"
+	"github.com/joho/godotenv"
 )
 
 var Version = "v0.0.0-dev"
 
 func main() {
-	versionFlag := flag.Bool("version", false, "Print version information")
-	repo := flag.String("repo", "", "Repository to operate on (e.g. owner/repo)")
-
-	flag.Parse()
-
-	if *versionFlag {
-		fmt.Printf("gh-pr-formatter version %s\n", Version)
-		os.Exit(0)
+	godotenv.Load()
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "GITHUB TOKEN not set")
+		os.Exit(1)
 	}
-
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
-	s.Suffix = " PR fetching..."
-	s.Writer = os.Stderr
-	s.Start()
-	defer s.Stop()
 
 	for _, arg := range os.Args[1:] {
 		if strings.HasPrefix(arg, "-") && !strings.HasPrefix(arg, "--") {
@@ -41,6 +35,45 @@ func main() {
 		}
 	}
 
+	// version check
+	versionFlag := flag.Bool("version", false, "Print version information")
+	repo := flag.String("repo", "", "Repository to operate on (e.g. owner/repo)")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("gh-pr-formatter version %s\n", Version)
+		os.Exit(0)
+	}
+
+	// loader start
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Suffix = " PR fetching..."
+	s.Writer = os.Stderr
+	s.Start()
+	defer s.Stop()
+
+	owner, repoName, err := repository.ResolveRepo(*repo)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"Error: '%s'\n", err)
+		os.Exit(1)
+	}
+
+	c := client.NewClient(token)
+	prs, err := c.ListMergedPullRequests(owner, repoName, 50)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"Error: '%s'\n", err)
+		os.Exit(1)
+	}
+
+	if len(prs) == 0 {
+		s.Stop()
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "No merged pull requests found on 'develop' since last 'main' merge.")
+		os.Exit(0)
+	}
+
 	if err := gh.CheckEnvironment(*repo); err != nil {
 		s.Stop()
 		fmt.Fprintln(os.Stderr)
@@ -48,15 +81,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	mergedPr, err := gh.GetPrList(*repo)
-	if err != nil {
-		s.Stop()
-		fmt.Fprintln(os.Stderr)
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	groupedPrs := utils.GroupedPrsByAuthor(mergedPr)
+	groupedPrs := pr.GroupedPrsByAuthor(prs)
 	fileName, err := template.BuildMarkdown(groupedPrs)
 	if err != nil {
 		s.Stop()
