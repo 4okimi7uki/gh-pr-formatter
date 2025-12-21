4b078f7658d4dcca4d29c882c4cdde82ffe1191e
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-12-21 14:02:24 +0900

    refactor: logo function

--- internal/ui/logo.go
+++ internal/ui/logo.go
@@ -8,13 +8,13 @@ import (
 	"github.com/muesli/termenv"
 )
 
-func gradientLine(s string, leftHex, rightHex string) string {
+func gradientLine(s string, leftHex, rightHex string, maxLen int) string {
 	profile := termenv.ColorProfile()
 	c1, _ := colorful.Hex(leftHex)
 	c2, _ := colorful.Hex(rightHex)
 
 	runes := []rune(s)
-	n := len(runes)
+	n := maxLen
 	if n == 0 {
 		return s
 	}
@@ -46,9 +46,55 @@ func PrintLogo() {
  |_____|            |__|
 `
 
-	for t := range strings.SplitSeq(logo, "\n") {
-		fmt.Print(gradientLine(string(t)+"\n", "#F4307F", "#F9F339"))
+	const (
+		startColor = "#F4307F"
+		endColor   = "#F9F339"
+		tagline    = "make your release work a little bit easier :P"
+		repoURL    = "https://github.com/4okimi7uki/gh-pr-formatter"
+	)
+
+	logoLines := strings.Split(strings.TrimRight(logo, "\n"), "\n")
+	extraLines := []string{
+		"",
+		" " + tagline,
+		"",
+		"" + repoURL + "",
+		"",
 	}
-	fmt.Print(LightOrange.Sprint(" make your release work a little bit easier :P\n"))
+
+	all := append(append([]string{}, logoLines...), extraLines...)
+	width := maxLen(all)
+
+	// logo AA
+	for _, line := range logoLines {
+		fmt.Print(gradientLine(line+"\n", startColor, endColor, width))
+	}
+
+	// tagline
+	fmt.Print(gradientLine("\n "+tagline+"\n", startColor, endColor, width))
+
+	// upperBar
+	periodBar := strings.Repeat(".", len(repoURL)+3)
+	fmt.Println(gradientLine(periodBar, startColor, endColor, width))
+
+	// URL
+	fmt.Print(gradientLine(" "+repoURL+"\n", startColor, endColor, len(repoURL)))
+
+	// lowerBar
+	dotBar := strings.Repeat("·", len(repoURL)+3)
+	fmt.Println(gradientLine(dotBar, startColor, endColor, width))
 	fmt.Println()
+
+	fmt.Println()
+}
+
+func maxLen(lines []string) int {
+	max := 0
+	for _, s := range lines {
+		s = strings.TrimRight(s, "\r")
+		if max < len(s) {
+			max = len(s)
+		}
+	}
+	return max
 }
--- internal/ui/print.go
+++
@@ -1,9 +0,0 @@
-package ui
-
-import (
-	"github.com/fatih/color"
-)
-
-func PrintlnNoErr(c *color.Color, s string) {
-	_, _ = c.Println(s)
-}
